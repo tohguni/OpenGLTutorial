49d3b5a050c39bcc721d1eeb640d176e775c82be
tohguni <ohguni.toshihiro@gmail.com>
2013-01-20 16:44:59 +0900

    OpenGLTutorial2 1.Some modules

---
+++ Display.hs
@@ -0,0 +1,11 @@
+module Display (display) where
+
+--import Graphics.Rendering.OpenGL
+import Graphics.UI.GLUT
+
+import Cube
+
+display = do
+  clear [ColorBuffer]
+  cube (0.2::GLfloat)
+  flush
--- HelloWorld.hs
+++ HelloWorld.hs
@@ -1,8 +1,7 @@
 --import Graphics.Rendaring.OpenGL
 import Graphics.UI.GLUT
 
-myPoints :: [(GLfloat,GLfloat,GLfloat)]
-myPoints = map (\k -> (sin(2*pi*k/12),cos(2*pi*k/12),0.0)) [1..12]
+import Bindings
 
 main :: IO ()
 main = do
@@ -10,35 +9,5 @@ main = do
   createWindow "Hello World"
   displayCallback $= display
   reshapeCallback $= Just reshape
+  keyboardMouseCallback $= Just keyboardMouse
   mainLoop
-
-reshape s@(Size w h) = do
-  viewport $= (Position 0 0, s)
-  postRedisplay Nothing
-
-display :: IO ()
-display = do
-  clear [ ColorBuffer ]
-  --renderPrimitive Polygon $ mapM_ (\(x,y,z) -> vertex$Vertex3 x y z) myPoints
-  renderPrimitive Quads $ do
-    color $ (Color3 (1.0::GLfloat) 0 0)
-    vertex $ (Vertex3 (0::GLfloat) 0 0)
-    vertex $ (Vertex3 (0::GLfloat) 0.2 0)
-    vertex $ (Vertex3 (0.2::GLfloat) 0.2 0)
-    vertex $ (Vertex3 (0.2::GLfloat) 0 0)
-    color $ (Color3 (0::GLfloat) 1 0)
-    vertex $ (Vertex3 (0::GLfloat) 0 0)
-    vertex $ (Vertex3 (0::GLfloat) (-0.2) 0)
-    vertex $ (Vertex3 (0.2::GLfloat) (-0.2) 0)
-    vertex $ (Vertex3 (0.2::GLfloat) 0 0)
-    color $ (Color3 (0::GLfloat) 0 1)
-    vertex $ (Vertex3 (0::GLfloat) 0 0)
-    vertex $ (Vertex3 (0::GLfloat) (-0.2) 0)
-    vertex $ (Vertex3 ((-0.2)::GLfloat) (-0.2) 0)
-    vertex $ (Vertex3 ((-0.2)::GLfloat) 0 0)
-    color $ (Color3 (1::GLfloat) 0 1)
-    vertex $ (Vertex3 (0::GLfloat) 0 0)
-    vertex $ (Vertex3 (0::GLfloat) 0.2 0)
-    vertex $ (Vertex3 ((-0.2::GLfloat)) 0.2 0)
-    vertex $ (Vertex3 ((-0.2::GLfloat)) 0 0)
-  flush
