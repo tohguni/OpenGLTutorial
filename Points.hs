ded08ec8a42f38c8efcabdab56d239a69ff1dc02
tohguni <ohguni.toshihiro@gmail.com>
2013-01-20 18:14:40 +0900

    OpenGLTutorial2 2.Local transformations

---
+++ Points.hs
@@ -0,0 +1,7 @@
+module Points where
+
+import Graphics.UI.GLUT
+
+points :: Int -> [(GLfloat,GLfloat,GLfloat)]
+points n' = let n = fromIntegral n' in
+              map (\k -> let t = 2*pi*k/n in (sin(t),cos(t),0.0)) [1..n]
