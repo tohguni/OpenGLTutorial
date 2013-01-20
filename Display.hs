ad952417fd60df5b9e336aa8a33e9878684bdcc5
tohguni <ohguni.toshihiro@gmail.com>
2013-01-20 18:55:55 +0900

    OpenGL Tutorial2 3 Back to the callbacks

--- Display.hs
+++ Display.hs
@@ -1,17 +1,30 @@
-module Display (display) where
+module Display (display, idle) where
 
 --import Graphics.Rendering.OpenGL
 import Graphics.UI.GLUT
+import Data.IORef
 
 import Cube
 import Points
 
-display = do
+display angle position = do
   clear [ColorBuffer]
-  scale 0.7 0.7 (0.7::GLfloat)
-  mapM_ (\(x,y,z) -> preservingMatrix $ do
-    color $ Color3 ((x+1.0)/2.0) ((y+1.0)/2.0) ((z+1.0)/2.0)
-    translate $ Vector3 x y z
-    cube (0.1::GLfloat)
-    ) $ points 7
-  flush
+  loadIdentity
+  (x,y) <- get position
+  translate $ Vector3 x y (0::GLfloat)
+  preservingMatrix $ do
+    a <- get angle
+    rotate a $ Vector3 0 0 (1::GLfloat)
+    scale 0.7 0.7 (0.7::GLfloat)
+    mapM_ (\(x,y,z) -> preservingMatrix $ do
+      color $ Color3 ((x+1.0)/2.0) ((y+1.0)/2.0) ((z+1.0)/2.0)
+      translate $ Vector3 x y z
+      cube (0.1::GLfloat)
+      ) $ points 7
+  swapBuffers
+
+idle angle delta = do
+  a <- get angle
+  d <- get delta
+  angle $=! (a + d)
+  postRedisplay Nothing
--- HelloWorld.hs
+++ HelloWorld.hs
@@ -1,13 +1,19 @@
 --import Graphics.Rendaring.OpenGL
 import Graphics.UI.GLUT
+import Data.IORef
 
 import Bindings
 
 main :: IO ()
 main = do
   (progname, _) <- getArgsAndInitialize
+  --initialDisplayMode $= [DoubleBuffered]
   createWindow "Hello World"
-  displayCallback $= display
   reshapeCallback $= Just reshape
-  keyboardMouseCallback $= Just keyboardMouse
+  angle <- newIORef (0.0::GLfloat)
+  delta <- newIORef (0.1::GLfloat)
+  position <- newIORef (0.0::GLfloat, 0.0)
+  keyboardMouseCallback $= Just (keyboardMouse delta position)
+  idleCallback $= Just (idle angle delta)
+  displayCallback $= (display angle position)
   mainLoop
